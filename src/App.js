73c12deda77934c50adae54226292f3468954042
Andréia Crystina <andreia.crystina@hotmail.com>
2019-12-25 22:08:28 -0300

    Rocketseat GoStack 9.0 - Hooks-app - Aplicação desenvolvida durante o curso de ReactJS e React Native sobre o uso de React Hooks.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useState, useEffect, useMemo, useCallback,
+} from 'react';
 
 function App() {
+  const [newTech, setNewTech] = useState('');
+  const [techs, setTechs] = useState([]);
+  const techsSize = useMemo(() => techs.length, [techs]);
+
+  useEffect(() => {
+    const techs = JSON.parse(localStorage.getItem('techs'));
+
+    if (techs) {
+      setTechs(techs);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  const handleAdd = useCallback(() => {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        { techs.map(tech => (
+          <li key={ tech }>{ tech }</li>
+        )) }
+      </ul>
+
+      <p><strong>Você tem { techsSize } tecnologias.</strong></p>
+
+      <input type="text" value={ newTech } onChange={ ({ target }) => setNewTech(target.value) } />
+
+      <button type="button" onClick={ handleAdd }>Adicionar</button>
+    </>
   );
 }
 
